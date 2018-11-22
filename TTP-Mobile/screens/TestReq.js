68e9153e1fa196ecd4f1ffcd1c0a77642fa39a78
Kristin <kristinnharper@gmail.com>
2018-11-21 20:45:37 -0600

    forgot to save a few

--- TTP-Mobile/screens/TestReq.js
+++ TTP-Mobile/screens/TestReq.js
@@ -1,7 +1,27 @@
 var client = require("../twitterAPI");
 
-client.get("favorites/list", function(error, tweets, response) {
-  if (error) throw error;
-  console.log(tweets); // The favorites.
-  console.log(response); // Raw response object.
+// client.get("favorites/list", function(error, tweets, response) {
+//   if (error) throw error;
+//   console.log(tweets); // The favorites.
+//   console.log(response); // Raw response object.
+// });
+
+var stream = client.stream("statuses/filter", { track: "javascript" });
+stream.on("data", function(event) {
+  console.log(event && event.text);
+});
+
+stream.on("error", function(error) {
+  throw error;
+});
+
+// You can also get the stream in a callback if you prefer.
+client.stream("statuses/filter", { track: "javascript" }, function(stream) {
+  stream.on("data", function(event) {
+    console.log(event && event.text);
+  });
+
+  stream.on("error", function(error) {
+    throw error;
+  });
 });
